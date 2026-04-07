89724601bd77463a7ccfc08705e662b78a3fc654
pappadf <95528397+pappadf@users.noreply.github.com>
2026-04-07 21:33:08 +0000

    Adding first A/UX 3.0 integration test case

---
+++ tests/integration/se30-aux-3/config.mk
@@ -0,0 +1,11 @@
+# Integration test configuration: SE/30 A/UX 3.0.1 Boot
+# Verifies that the SE/30 boots from the A/UX 3.0.1 Installation Boot Disk
+
+TEST_NAME := SE/30 A/UX 3.0.1 Boot
+TEST_DESC := Boots SE/30 with Universal ROM from A/UX 3.0.1 Installation Boot Disk
+
+# Universal ROM shared by SE/30, IIcx, IIx (checksum 0x97221136)
+TEST_ROM := roms/SE30.rom
+
+# Boot from A/UX 3.0.1 Installation Boot Disk (floppy image)
+TEST_ARGS := fd=$(TEST_DATA)/aux/aux_3.0.1/AUX_Installation_Boot_Disk.img
